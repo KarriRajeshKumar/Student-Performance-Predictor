5fcbd944c9514d3ee03b88085a98cefb30f955e7
Karri Rajesh Kumar <145736377+KarriRajeshKumar@users.noreply.github.com>
2025-03-13 14:40:21 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify, render_template
+import pickle
+
+app = Flask(__name__)
+
+
+with open('model.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json() 
+    features = data['features']  
+    
+    
+    prediction = model.predict([features])  
+    if prediction > 100:
+        prediction = 100
+    
+    
+    return jsonify({'prediction': prediction[0]})  
+
+if __name__ == '__main__':
+    app.run(debug=True)
